d2b62ba9164dd49060a238068c78299a3000b58e
samirabarrera <samira.barrera@galileo.edu>
2025-05-20 21:29:21 -0600

    fixing jwt

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import express from 'express';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 import { Pool } from './basede_Datos';
-import { JsonWebTokenError } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs';
 
 const app = express();
@@ -17,45 +17,34 @@ const authMiddleware = (req, res, next) => {
     }
 }
 
-const token = authHeader.split(' ')[1];
-if (!token) return res.status(403).json({ mensaje: "Token no proporcionado" });
-try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.usuario = decoded.data;
-    next();
-} catch (error) {
-    return res.status(401).json({ mensaje: "Token inválido o expirado"});
-}
-
 //REGISTRO DE USUARIOS POR RUTA PROTEGIDA POR JWT
-app.post('/registro', verifyToken, async (req, res) => {
+app.post('/registro/users', verifyToken, async (req, res) => {
     const { nombre, correo, contraseña } = req.body;
     if (!nombre || !correo || !contraseña) {
-        return res.status(400).json({ mensaje: 'Falta llenar un campo' });
+        return res.send(404).json({ mensaje: 'Falta llenar un campo' });
     }
 })
 
 try {
     const { rows } = await Pool.query('SELECT * FROM usuarios WHERE correo = $1', [correo]);
     if (rows.length > 0) {
-        return res.status(409).json({ mensaje: 'El correo ya está en uso' });
+        return res.send(404).json({ mensaje: 'Ya existe un usuario con ese correo' });
     }
     const hash = await bcrypt.hash(contraseña, 10);
     await pool.query('INSERT INTO usuarios (nombre, correo, contraseña) VALUES ($1, $2, $3)', [nombre, correo, hash]);
-    res.status(201).json({ mensaje: 'Usuario registrado con éxito' });
+    res.status(404).json({ mensaje: 'Usuario registrado con éxito' });
 } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: 'Error en el servidor' });
 }
 
 //RUTA DE LOGIN PROTEGIDA
-app.get('/login/:users', authMiddleware, async (req, res) => {
+app.get('/login/users', authMiddleware, async (req, res) => {
     const { correo, contraseña } = req.body;
      if (!correo || !contraseña) {
         return res.status(400).json({ mensaje: "Todos los campos son obligatorios" });
     }
 });
-
 
 
 app.listen(port, () => {
